10e4f6febce89335058814034be691bde283f176
Samuel Hernandez <sam@samhza.com>
2024-02-26 14:45:04 -0500

    day9

---
+++ day9.hs
@@ -0,0 +1,25 @@
+import Text.ParserCombinators.ReadP
+import Data.Char as C ( isDigit )
+import Data.Semigroup
+
+main = do
+    contents <- getContents
+    let [(ns, _)] = readP_to_S input contents
+    print $ getSum $ foldMap (Sum . next) ns
+    print $ getSum $ foldMap (Sum . next . reverse) ns
+
+next [x,y] | x == y = x
+next xs = last xs + next (diff xs)
+    where
+        diff (x:y:xs) = y - x : diff (y:xs)
+        diff _ = []
+
+input =
+    sepBy line (char '\n') <*
+    char '\n' <*
+    eof
+    
+line = sepBy number (char ' ')
+
+number :: ReadP Int
+number = read <$> many1 (satisfy (\x-> x=='-' || C.isDigit x))
